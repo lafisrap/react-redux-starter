c7769bdb331bfd6dd7757ecd8becbd72042c393b
Michael <michael@c2064.org>
2017-09-12 13:41:40 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
-import App from './components/App'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css'
+import './index.css';
 
-const target = document.querySelector('#root')
+const target = document.querySelector('#root');
 
 render(
   <Provider store={store}>
@@ -21,6 +21,6 @@ render(
     </ConnectedRouter>
   </Provider>,
   target
-)
+);
 
 registerServiceWorker();
